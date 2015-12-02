9b0ddb9a17002c058f0c64eb852d774978d90d40
TurtleHx <TurtleHx@users.noreply.github.com>
2015-12-02 01:42:05 -0800

    Initial commit

---
+++ Bioshock Physics Unlocker/main.cpp
@@ -0,0 +1,115 @@
+#include <fstream>
+#include <iostream>
+#include <sstream>
+#include <chrono>
+#include <thread>
+#include <sys/stat.h>
+
+#define BIOSHOCK "Bioshock.exe"
+#define BACKUP "Bioshock_Backup.exe"
+#define TIMESTEP_SIGNATURE "\x48\x6B\x55\x6E\x72\x65\x61\x6C\x43\x68\x61\x72\x61\x63\x74\x65\x72\x50\x6F\x69\x6E\x74\x43\x6F\x6C\x6C\x65\x63\x74\x6F\x72\x40\x40\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x2E\x3F\x41\x56\x46\x52\x75\x6E\x6E\x61\x62\x6C\x65\x40\x40"
+#define TIMESTEP_MASK "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx???????????????xxxxxxxxxxxxxxx"
+#define TIMESTEP_OFFSET 36
+#define MESSAGE_SLEEP_TIME 1000
+#define ERROR_SLEEP_TIME 7000
+
+struct ScanResult {
+	bool found;
+	std::streampos location;
+};
+
+ScanResult scanPatchLocation(std::fstream &file, const char *search, const char *mask, unsigned int offset) {
+	std::stringstream strings;
+	strings << file.rdbuf();
+	std::string fileString = strings.str();
+	
+	ScanResult patch;
+	size_t fileLength = fileString.length();
+	size_t sigLength = strlen(mask);
+
+	for (unsigned int i = 0; i < fileLength - sigLength; i++) {
+		for (unsigned int j = 0; j < sigLength; j++) {
+			if (mask[j] == '?') { continue; }
+			if (search[j] == fileString.at(i + j)) {
+				if (j == sigLength - 1) {
+					patch.found = true;
+					patch.location = i + offset;
+					return patch;
+				}
+			}
+			else {
+				break;
+			}
+		}
+	}
+
+	patch.found = false;
+	patch.location = 0;
+	return patch;
+}
+
+int main() {
+	// Try to locate Bioshock
+	std::cout << "Opening Bioshock executable...";
+	std::fstream bsBinary(BIOSHOCK, std::ios::binary | std::ios::out | std::ios::in);
+
+	if (!bsBinary.good()) {
+		std::cerr << "Error! Could not find the Bioshock executable. Is the program in the right folder?" << std::endl;
+		std::this_thread::sleep_for(std::chrono::milliseconds(ERROR_SLEEP_TIME));
+		return EXIT_FAILURE;
+	}
+	else {
+		std::cout << "Success!" << std::endl;
+	}
+
+	// Find the location in the exe to patch
+	std::cout << "Scanning for location to patch...";
+	ScanResult timestepPatch = scanPatchLocation(bsBinary, TIMESTEP_SIGNATURE, TIMESTEP_MASK, TIMESTEP_OFFSET);
+	if (!timestepPatch.found) {
+		std::cerr << "Error! Could not find the physics timestep location to patch within the executable." << std::endl;
+		std::this_thread::sleep_for(std::chrono::milliseconds(ERROR_SLEEP_TIME));
+		return EXIT_FAILURE;
+	}
+	else {
+		std::cout << "Success!" << std::endl;
+	}
+
+	// Make a backup if we don't already have one.
+	struct stat buf;
+	if (stat(BACKUP, &buf) != 0) {
+		std::cout << "Making a backup of Bioshock...";
+		std::ofstream saveBackup(BACKUP, std::ios::binary);
+		if (saveBackup.good()) {
+			bsBinary.seekg(0);
+			saveBackup << bsBinary.rdbuf();
+			std::cout << "Success!" << std::endl;
+		}
+		else {
+			std::cerr << "WARNING: Could not create backup!" << std::endl;
+		}
+	}
+
+	// Show the user what the timestep is set to right now.
+	float curTimestep;
+	bsBinary.seekg(timestepPatch.location);
+	bsBinary.read((char*)&curTimestep, sizeof(float));
+	std::cout << "The physics FPS is currently: " << (1.0 / curTimestep) << " FPS" << std::endl;
+
+	// User input so they can change it.
+	std::string inputLine;
+	int inFPS;
+	while ((std::cout << "New physics FPS: ") && std::getline(std::cin, inputLine) && !(std::istringstream{ inputLine } >> inFPS)) {
+		std::cerr << "Invalid choice." << std::endl;
+	}
+
+	// Actually replace it in the file
+	float desiredTimestep = 1.0f / inFPS;
+	bsBinary.seekp(timestepPatch.location);
+	bsBinary.write((char*)&desiredTimestep, sizeof(float));
+
+	// cool.
+	std::cout << "Updated physics to " << inFPS << " FPS!" << std::endl;
+	std::this_thread::sleep_for(std::chrono::milliseconds(MESSAGE_SLEEP_TIME));
+
+	return EXIT_SUCCESS;
+}
